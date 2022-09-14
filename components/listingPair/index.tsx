6883ffa9b63e1f415eee2db04d8829edece2d7dd
Joe Ellul-Turner <joeet@pop-os.localdomain>
2022-09-14 14:13:52 +0100

    countdown timer and button disable once clicked

--- components/listingPair/index.tsx
+++ components/listingPair/index.tsx
@@ -3,21 +3,50 @@ import { useEffect, useState } from "react";
 export const ListingPair = () => {
   const [listingPair, setListingPair] = useState<any>();
 
+  const fetchListingPair = async () => {
+    const req = await fetch(
+      "https://api.binance.com/api/v3/avgPrice?symbol=BTCUSDT"
+    );
+    const res = await req.json();
+    setListingPair(res);
+  };
+
+  const [isDisabled, setIsDisabled] = useState(false);
+  const [counter, setCounter] = useState(60);
+
+  const handleClick = () => {
+    fetchListingPair();
+    setIsDisabled(true);
+  };
+
   useEffect(() => {
-    const fetchDeviceIds = async () => {
-      const req = await fetch(
-        "https://api.binance.com/api/v3/avgPrice?symbol=BTCUSDT"
-      );
-      const res = await req.json();
-      setListingPair(res);
-    };
-    setInterval(() => fetchDeviceIds(), 60000);
-    fetchDeviceIds();
+    if (isDisabled == true) {
+      setTimeout(() => setIsDisabled(false), 60000);
+    }
+  }, [isDisabled]);
+
+  if (counter == 0) setCounter(60);
+
+  if (isDisabled == true) setTimeout(() => setCounter(counter - 1), 1000);
+
+  useEffect(() => {
+    fetchListingPair();
+    setInterval(() => fetchListingPair(), 60000);
   }, []);
 
-  console.log(listingPair);
+  console.log(isDisabled);
 
-  return <>
-  <div>{listingPair && listingPair.price}</div>
-  </>;
+  return (
+    <>
+      <div>{listingPair && listingPair.price}</div>
+      <div>{counter == 60 ? null : counter}</div>
+      <button
+        disabled={isDisabled}
+        onClick={handleClick}
+        className="bg-gray-500"
+      >
+        click
+      </button>
+    </>
+  );
 };
