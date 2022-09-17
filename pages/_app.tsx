a713728b0a0a2b8c1b2d17a9207aa974a14347ee
Joe Ellul-Turner <joeet@pop-os.localdomain>
2022-09-17 18:17:45 +0100

    removed configs

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,6 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import Amplify from 'aws-amplify';
-import config from '../src/aws-exports.js';
-Amplify.configure(config);
+
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
